ed876d6cb463f189e7f99d449a822ba975f2eb6d
Jase Owens <jaseowens197@gmail.com>
2022-07-21 10:59:24 -0500

    Basic functions in place

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export const Woah = () => {
-  console.log("hello");
+import { EventEmitter } from "events";
+import { clipboard, Clipboard } from "electron";
+
+//Types
+type ListeningClipboardEvents = "new-text";
+
+interface WatchClipboard extends Clipboard {
+  startWatching?: (interval?: number) => void;
+  on?: (event: ListeningClipboardEvents, cb: (newText: string) => void) => void;
+}
+
+//Globals
+export const ListeningClipboard: WatchClipboard = clipboard;
+let intervalId: NodeJS.Timer;
+let currentText: string = clipboard.readText();
+const clipboardEvents = new EventEmitter();
+
+//Functions
+ListeningClipboard.on = (event, cb) => {
+  clipboardEvents.on(event, cb);
+};
+
+ListeningClipboard.startWatching = (checkInterval: number = 750) => {
+  if (!intervalId)
+    intervalId = setInterval(() => {
+      let newText = clipboard.readText();
+      if (compareText(currentText, newText)) {
+        currentText = newText;
+        clipboardEvents.emit("new-text", newText);
+      }
+    }, checkInterval);
+};
+
+//Helpers
+const compareText = (inputA?: string, inputB?: string) => {
+  if (!inputA || !inputB) return false;
+  return inputA !== inputB;
 };
